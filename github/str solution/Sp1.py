ee0ffc2ae70214d26f759d50f87d2c9a13ddf0a3
22bca236 <127925661+22bca236@users.noreply.github.com>
2023-04-04 22:05:07 +0530

    Delete github directory

--- github/str solution/Sp1.py
+++
@@ -1,7 +0,0 @@
-#python script to check string is palindrome or not
-a=input("Enter string :")
-b=a[::-1]
-if(a==b):
-    print(f"String {a} is palindrome !")
-else:
-    print(f"String {a} is not palindrome !")
