8d8afbf8887d5f537c53a46e2aa24f04e31877f3
22bca236 <127925661+22bca236@users.noreply.github.com>
2023-04-04 22:18:03 +0530

    Update Sp5.py

--- str solution/Sp5.py
+++ str solution/Sp5.py
@@ -1,14 +1,14 @@
-def symmetric(lis):
+def symmet(lis):
     h=(len(lis)//2)
     f=lis[:h]
     s=lis[h:]
     if f==s:
-        print(f"String {} is symmetric!!!!".format(lis))
+        print("String {} is symmetric!!!!".format(lis))
     else:
         print("String {} is not symmetric!!!".format(lis))
 
 
 
 a=input("Enter a string:")
-symmetric(a)
+symmet(a)
           
